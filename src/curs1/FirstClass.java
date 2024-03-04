d3abf7f81758ad90b347f44127862ec67e5d1d34
unknown <irinaandrei83@gmail.com>
2024-03-04 11:50:19 +0200

    curs1

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,19 @@
+package curs1;
+
+public class FirstClass { 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Test");
+		FirstClass obiect = new FirstClass();
+		obiect.greetings();
+		
+		
+		
+	}
+
+	public void greetings() {
+		System.out.println("Salut");
+}
+}
